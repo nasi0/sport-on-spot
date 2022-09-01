b10ad4e60b92e9721170cee5ed720ec12d3a7fd0
Atanas Petrov <nasi0.ap@gmail.com>
2022-09-01 07:44:19 +0300

    Many pages. Services.

--- src/app/pages/match/match.component.ts
+++ src/app/pages/match/match.component.ts
@@ -55,6 +55,6 @@ export class MatchComponent implements OnInit {
 		}
 		this.currentMatch.result = result;
 		this.currentMatch.status = 3;
-		this.matchService.finalizeMatch(this.currentMatch).subscribe((match) => this.currentMatch = match);
+		this.matchService.finalizeMatch(this.currentMatch).subscribe((match) => console.log('Success'));
 	};
 }
